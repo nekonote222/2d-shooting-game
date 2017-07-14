f210b91ab9b9f47251811f29fefee6727b9d5e86
taga <g1344766@cse.kyoto-su.ac.jp>
2017-07-14 15:36:06 +0900

    06 背景

---
+++ Assets/Scripts/DestroyArea.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyArea : MonoBehaviour {
+
+    void OnTriggerExit2D(Collider2D c)
+    {
+        Destroy(c.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
